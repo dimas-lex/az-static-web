d8f91cdc551d27a6abbbd488945dc39d80a770d8
dimas <dimaslex@gmail.com>
2021-11-15 20:19:36 +0100

    Update API calls

--- src/features/weather/weatherApi.ts
+++ src/features/weather/weatherApi.ts
@@ -1,5 +1,5 @@
 export const fetchWeather = async (country?: string, city?: string): Promise<any> => {
-  const responseTxt = await fetch(`https://api.weatherbit.io/v2.0/current?city=${city}&country=${country}&key=6730704688bd4e2f8f3d280f2732353a`);
+  const responseTxt = await fetch(`https://dimas-api-fn.azurewebsites.net/api/weather?city=${city}&country=${country}&key=123`);
   const response = await responseTxt.json();
 
   if (Array.isArray(response.data)) {
