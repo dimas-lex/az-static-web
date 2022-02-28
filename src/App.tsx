39b83edaf5f0ef454fc6b3f183df60d5f8ece8cd
dimas <dimaslex@gmail.com>
2022-02-28 09:38:45 +0100

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,10 @@
 import './App.css';
 import React  from 'react';
-import { Weather } from './components/Weather';
-import { Rates } from './components/Rates/Rates/Rates';
-import { selectIsAddRateVisible, selectSelectedRate } from './features/rates/ratesSlice';
-import { useAppSelector } from './app/hooks';
-import { RateDetail } from './components/Rates/RateDetail';
-import { selectIsLoading } from './app/selectors';
-import { ProgressBar } from './components/ProgressBar';
-import { AddRate } from './components/Rates/AddRate';
-import { HeaderBox } from './components/Header/HeaderBox';
-import { FontContext } from './app/context';
 
 function App() {
-  const selectedRate = useAppSelector(selectSelectedRate);
-  const isAddRateVisible = useAppSelector(selectIsAddRateVisible);
-  const isLoading = useAppSelector(selectIsLoading);
-
 
   return (
-    <FontContext.Provider value={12}>
-    <div className="App">
-      <ProgressBar isVisible={isLoading} />
-      <HeaderBox title="SOME TITLE" />
-      <Weather />
-      <div className="AppRates" >
-        <Rates />
-      </div>
-      {
-        selectedRate && (
-          <div className="AppRateDetail" >
-            <RateDetail rate={selectedRate} />
-          </div>
-        )
-      }
-
-      <AddRate isOpen={isAddRateVisible} />
-    </div>
-
-    </FontContext.Provider>
+    <div>Test</div>
   );
 }
 
